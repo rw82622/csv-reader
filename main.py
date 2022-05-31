a3f07e71ffc7b552549004597bbf994be45f9a46
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-31 15:58:42 -0500

    python solution to csv-reader

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+
+class Animals():
+    def __init__(self, userInput):
+        self.userInput = userInput
+        
+    def getData(self):
+        try:
+            with open(f"./data/{self.userInput}.csv") as f:
+                test = csv.reader(f)
+                for row in test:
+                    if row[0] != "name":
+                        print(f"{row[0]} is a{row[1]} year old{row[2]}")
+        except:
+            print(f"Sorry, we don't have any {self.userInput} here.")
+            
+
+animal1 = Animals("goat")
+animal1.getData()
