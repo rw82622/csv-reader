32cd019ae5362515ff1c022248aced19b75ffb37
Rexford Wiafe <w.rexford@yahoo.com>
2022-08-13 08:57:41 -0500

    final changes

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import csv
 
 class Animals():
-    def __init__(self, userInput):
-        self.userInput = userInput
+    def __init__(self, user_input):
+        self.user_input = user_input
         
     def getData(self):
         try:
-            with open(f"./data/{self.userInput}.csv") as f:
-                test = csv.reader(f)
-                for row in test:
+            with open(f"./data/{self.user_input}.csv") as f:
+                animal_data = csv.reader(f)
+                for row in animal_data:
                     if row[0] != "name":
                         print(f"{row[0]} is a{row[1]} year old{row[2]}")
         except:
-            print(f"Sorry, we don't have any {self.userInput} here.")
+            print(f"Sorry, we don't have any {self.user_input} here.")
             
 
 animal1 = Animals("dogs")
